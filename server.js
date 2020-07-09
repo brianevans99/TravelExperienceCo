478b75d537a9a2fe1b03bd81b2ca9b4d64c65545
Brian Evans <brianevans99@gmail.com>
2020-07-09 19:43:50 -0400

    updated server.js

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
-  app.use('/api/data', require('./routes/api/data'))
 }
+
+app.use('/api/data', require('./routes/api/data'))
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
