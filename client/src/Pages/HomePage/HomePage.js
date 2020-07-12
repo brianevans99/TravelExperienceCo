1c3f464bfeb3bf54fa129575f1374f710a148592
Brian Evans <brianevans99@gmail.com>
2020-07-12 12:53:06 -0400

    updated Home Page

--- client/src/Pages/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage.js
@@ -1,11 +1,7 @@
 import React, { Component, Fragment } from 'react'
 import FeelgoodSection from '../../Components/FeelgoodSection/FeelgoodSection'
-// import Updates from '../../Components/Updates/Updates'
 import VideoBlock from '../../Components/VideoBlock/VideoBlock'
-// import TravelFavorites from '../../Components/TravelFavorites/TravelFavorites'
-// import EnhancedVacation from '../../Components/EnhancedVacation/EnhancedVacation'
 import GroupTrip from '../../Components/GroupTrip/GroupTrip'
-// import FeaturedArticle from '../../Components/FeaturedArticle/FeaturedArticle'
 import Experience from '../../Components/Experience/Experience'
 import Accolades from '../../Components/Accolades/Accolades'
 import ImageSlider from '../../Components/ImageSlider/ImageSlider'
@@ -15,14 +11,9 @@ export default class Home extends Component {
     return (
       <Fragment>
         <ImageSlider />
-
-        {/* <EnhancedVacation /> */}
         <FeelgoodSection />
-        {/* <Updates /> */}
         <VideoBlock />
-        {/* <TravelFavorites /> */}
         <GroupTrip />
-        {/* <FeaturedArticle /> */}
         <Experience />
         <Accolades />
       </Fragment>
