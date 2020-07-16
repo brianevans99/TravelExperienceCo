c64ebb0247aa434c15c43e8b3713081ff240e358
Brian Evans <brianevans99@gmail.com>
2020-07-16 19:27:40 -0400

    everything connected except MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 // const connectDB = require('./config/database')
 const path = require('path')
-// const morgan = require('morgan')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -11,17 +11,17 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 //Init middleware
-// app.use(morgan('dev'))
-// app.use(express.json({ extended: false }))
+app.use(morgan('dev'))
+app.use(express.json({ extended: false }))
 
-// app.get('/', (req, res, next) => {
-//   res.json({
-//     message: 'backend connection good',
-//   })
-// })
+app.get('/', (req, res, next) => {
+  res.json({
+    message: 'backend connection good',
+  })
+})
 
 //Define routes
-// app.use('/api/caribbean', require('./api/routes/caribbean'))
+app.use('/api/caribbean', require('./api/routes/caribbean'))
 
 //serve static assets in production
 if (process.env.NODE_ENV === 'production') {
